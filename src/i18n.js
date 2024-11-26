071feab04d5e2e5dbb798afb45afc34215b17e75
Andreas Georgiou <andreasg046@gmail.com>
2024-11-27 01:36:21 +0200

    updates

--- src/i18n.js
+++ src/i18n.js
@@ -8,7 +8,7 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 // for passing in lng and translations on init
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/)
   // learn more: https://github.com/i18next/i18next-http-backend
   // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
   .use(Backend)
@@ -21,7 +21,6 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     backend: {
-      loadPath: './locales/{{lng}}/{{ns}}.json',
     },
     detection: {
       order: ['querystring', 'cookie', 'localStorage', 'sessionStorage'],
